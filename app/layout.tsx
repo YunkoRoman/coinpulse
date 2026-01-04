e28cf6ba6646d9f725220be42d45a589f9e5be00
Roman <romanyunko1996@gmail.com>
2026-01-04 16:19:15 +0200

    feat: Set up project with Prettier, add core components and types, and update assets.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/componets/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,10 +14,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'CoinPulse',
-  description: 'Crypto Screener App with a built-in High-Frequency Terminal & Dashboard',
+  title: "CoinPulse",
+  description: "Crypto Screener App with a built-in High-Frequency Terminal & Dashboard",
 };
-
 
 export default function RootLayout({
   children,
@@ -24,10 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className="dark">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
